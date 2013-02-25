4a24d5a2261ae9697398bc61db8eddbac1034b15
eivindw <eivindw@gmail.com>
2013-02-25 22:25:02 +0100

    Add dependency for looking up services using a client.

---
+++ src/test/java/eivindw/efergy/EfergyClientTest.java
@@ -0,0 +1,11 @@
+package eivindw.efergy;
+
+import org.junit.Test;
+
+public class EfergyClientTest {
+
+   @Test
+   public void connectClientWithSession() {
+      System.out.println("TODO");
+   }
+}
