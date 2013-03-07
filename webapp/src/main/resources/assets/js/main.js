417fed46170334813d5312a7d0b717228a2fdc56
Eivind B Waaler <eivind.waaler@bekk.no>
2013-03-07 16:29:47 +0100

    Try to get crossroads.js working.

--- webapp/src/main/resources/assets/js/main.js
+++ webapp/src/main/resources/assets/js/main.js
@@ -1,39 +1,56 @@
 requirejs.config({
-    urlArgs: 'v=' + Math.random(),
+   urlArgs: 'v=' + Math.random(),
 
-    paths: {
-        'jquery': '../lib/jquery-1.9.1',
-        'underscore': '../lib/underscore-1.4.4',
-        'hogan': '../lib/hogan-2.0.0',
-        'hgn': '../lib/plugin/hgn',
-        'text': '../lib/plugin/text',
-        'highcharts': '../lib/Highcharts-2.3.5/js/highcharts',
-        'highcharts-more': '../lib/Highcharts-2.3.5/js/highcharts-more',
-        'bootstrap': '../lib/bootstrap/js/bootstrap',
-        'moment': '../lib/moment-2.0.0',
-        'moment-norwegian': '../lib/moment-nb'
-    },
+   paths: {
+      'jquery': '../lib/jquery-1.9.1',
+      'underscore': '../lib/underscore-1.4.4',
+      'hogan': '../lib/hogan-2.0.0',
+      'hgn': '../lib/plugin/hgn',
+      'text': '../lib/plugin/text',
+      'highcharts': '../lib/Highcharts-2.3.5/js/highcharts',
+      'highcharts-more': '../lib/Highcharts-2.3.5/js/highcharts-more',
+      'bootstrap': '../lib/bootstrap/js/bootstrap',
+      'moment': '../lib/moment-2.0.0',
+      'moment-norwegian': '../lib/moment-nb',
+      'signals': '../lib/signals',
+      'crossroads': '../lib/crossroads.min',
+      'hasher': '../lib/hasher.min'
+   },
 
-    shim: {
-        'underscore': {
-            exports: '_'
-        },
-        'bootstrap': ['jquery'],
-        'highcharts': {
-            exports: 'Highcharts'
-        },
-        'highcharts-more': {
-            deps: ['highcharts'],
-            exports: 'HighchartsMore'
-        }
-    }
+   shim: {
+      'underscore': {
+         exports: '_'
+      },
+      'bootstrap': ['jquery'],
+      'highcharts': {
+         exports: 'Highcharts'
+      },
+      'highcharts-more': {
+         deps: ['highcharts'],
+         exports: 'HighchartsMore'
+      },
+      'crossroads': ['signals'],
+      'hasher': ['signals']
+   }
 });
 
-define(function(require) {
-    var $ = require('jquery');
-    require('bootstrap');
+define(function (require) {
+   var $ = require('jquery');
+   require('bootstrap');
+   var crossroads = require('crossroads');
+   var hasher = require('hasher');
 
-    $(document).ready(function() {
-        console.log("JavaScript startet!");
-    });
+   $(document).ready(function () {
+      crossroads.addRoute('about', function() {
+         console.log("About!");
+      });
+
+      function parseHash(newHash, oldHash){
+         crossroads.parse(newHash);
+      }
+
+      hasher.initialized.add(parseHash); // parse initial hash
+      hasher.changed.add(parseHash); //parse hash changes
+      hasher.init();
+   });
 });
